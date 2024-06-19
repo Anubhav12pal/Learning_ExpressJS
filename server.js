1eeeb858a2240a2c3f94970292d36314e9315c58
Anubhav Pal <anubhavpal97@gmail.com>
2024-06-19 19:20:33 +0530

    Some minor changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use("/api/contacts", require(""));
+app.use("/api/contacts", require("./routs/contactRouts"));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
